91f4cf73ec91bcd05d31f4105fdeb217818da5e8
Aleksandr <...>
2018-02-26 19:48:57 +0100

    Small change

--- Game/Plane.h
+++ Game/Plane.h
@@ -1,5 +1,7 @@
 #pragma once
 #include"Functional.h";
+#include"Texture.h"
+#include"Shaders.h"
 namespace GAME
 {
 	namespace GUI
@@ -7,8 +9,10 @@ namespace GAME
 		class Plane :public Aux::OpenGL
 		{
 		private:
+			boost::shared_ptr<Texture>Tex;
+			boost::shared_ptr<OPENGL::Pipeline>GLSL;
 		public:
-			 void Draw(void);
+			 void Draw(void) override;
 		};
 	}
 }
