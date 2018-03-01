98b730fe3489ef0256d3995e1bb0ccd22f1838b7
Aleksandr <...>
2018-03-01 20:15:49 +0100

    stand 2018-03-01

--- Game/MainLoop.cpp
+++ Game/MainLoop.cpp
@@ -14,25 +14,38 @@ int main()
 	share<GAME::OPENGL::Pipeline>GLSL(new GAME::OPENGL::Pipeline(share<GAME::OPENGL::Shader>(new 
 		GAME::OPENGL::Shader(L"Shaders/Vert.glsl", 0, 0, L"Shaders/Geom.glsl", L"Shaders/Frag.glsl"))));
 	share<GAME::Texture>Tex(new GAME::Texture(L"SimpleText.png", GAME::Texture::NEAR));
-	share<GAME::GUI::SimpleText>Text(new GAME::GUI::SimpleText(Tex, GLSL));
+	share<GAME::GUI::SimpleText>Text(new GAME::GUI::SimpleText(Tex, GLSL)); 
 	Text->SetText("X X X");
 	Text->SetColor(0, 255, 0);
 	Text->SetScale(40);
 	Text->SetAlign(MIDDLE_LEFT);
-	GAME::FlipFlopButton KeyF9(Renderer.GetContext(), GLFW_KEY_F9);
-	Renderer.Add(Text);
+	Text->SetPos(200, 200);
+	glfwSetInputMode(Renderer.GetContext(), GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+	GAME::FlipFlopButton KeyE(Renderer.GetContext(), GLFW_KEY_E);
 	int i = 0;
 	float Koef = 0;
+	bool Flag=false;
 	while (true)
 	{
-		if (KeyF9.FlipFlop())
+		if (KeyE.FlipFlop())
 		{
-
-			Lua.Lib("Scripts/Lib/Vector.chunk");
+			if (Flag)
+			{
+				std::cout << "add ID" << std::endl;
+				Renderer.Add(1337,Text);
+				Flag = false;
+			}else
+			{
+				std::cout << "delete ID" << std::endl;
+				Renderer.Remove(1337);
+				Flag = true;
+			}
+			
+			/*Lua.Lib("Scripts/Lib/Vector.chunk");
 			Lua.Run(L"Scripts/Main.lua");
 
 			lua_getglobal(Lua.Kernel(), "init");
-			lua_call(Lua.Kernel(), 0, 0);
+			lua_call(Lua.Kernel(), 0, 0);*/
 		}
 		else if (Lua.isRunnable())
 		{
@@ -44,7 +57,7 @@ int main()
 			Text->SetText("FPS " + std::to_string(Renderer.GetFPS()));
 		}
 		Koef = float(i) / 200;
-		Text->SetColor(255-char(255.0*Koef), char(255.0 * Koef), 255);
+		//Text->SetColor(255-char(255.0*Koef), char(255.0 * Koef), 255);
 		Text->SetPos(Renderer.GetCursorPosition().x+45, Renderer.GetCursorPosition().y);
 		boost::this_thread::sleep_for(boost::chrono::milliseconds(1));
 		glfwPollEvents();
