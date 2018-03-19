2604519c0264a275e7ce8dd5ed06ea822e6208a3
Aleksandr <...>
2018-03-19 20:03:58 +0100

    add camera object

--- Game/Base.cpp
+++ Game/Base.cpp
@@ -1,50 +1,48 @@
 #include "Base.h"
 #include<boost\chrono.hpp>
 
-void GAME::BASE::OnWindowReshape(GLFWwindow *, int NewWidth, int NewHeight)
-{
-	Sync.lock();
-	Camera.Projection = glm::ortho(-NewWidth / 2, NewWidth/2, -NewHeight/2, NewHeight/2);
-	Sync.unlock();
-}
 
 void GAME::BASE::XRenderLoop(void)
 {
-	std::unordered_map< size_t, boost::shared_ptr<Aux::OpenGL>> IDRenderList;
-	std::unordered_set<boost::shared_ptr<Aux::OpenGL>, Aux::HASH>RenderList;
+	std::unordered_map< size_t, boost::shared_ptr<Aux::GameObject>> IDRenderList;
+	std::unordered_set<boost::shared_ptr<Aux::GameObject>, Aux::HASH>RenderList;
 	auto Stump=boost::chrono::high_resolution_clock::now();
 	unsigned long long FrameRate = 0;
 	glfwMakeContextCurrent(this->Context);
 	glEnable(GL_BLEND);
 	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_STENCIL_TEST);
+	glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+	glDepthFunc(GL_LESS);
 	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glClearColor(0., 0, 0, 1.);
-	{
-		auto modo=glfwGetVideoMode(glfwGetPrimaryMonitor());
-		glViewport(0, 0,modo->width, modo->height);
-	}
 	auto it = IDRenderList.end();
 	auto it2 = RenderList.end();
 	auto LocalCamera = Camera;
 	while (Run)
 	{
+		Aux::TextureObject::AnimTime = boost::chrono::time_point_cast<boost::chrono::milliseconds>(boost::chrono::high_resolution_clock::now()).time_since_epoch().count();
 		Stump = boost::chrono::high_resolution_clock::now();
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 		if (Sync.try_lock()&&OnChange)//if lock
 		{
 			OnChange = false;
 			IDRenderList = IDObjs;
 			RenderList = Objs;
 		}
-		LocalCamera = Camera;
-		Sync.unlock();	
+		{
+			int height, width; glfwGetWindowSize(Context, &width, &height);
+			glViewport(0, 0, width, height);
+		}
+		LocalCamera = this->Camera;
+		Sync.unlock();
 		for (auto it = IDRenderList.begin(); it != IDRenderList.end(); it++)
 		{
-			it->second->Draw(LocalCamera);
+			it->second->Draw(LocalCamera,CameraUBO);
 		}
 		for (auto it = RenderList.begin(); it != RenderList.end(); it++)
 		{
-			it->get()->Draw(LocalCamera);
+			it->get()->Draw(LocalCamera, CameraUBO);
 		}
 		//Frame in Nano
 		FrameRate = boost::chrono::duration_cast<boost::chrono::nanoseconds>(boost::chrono::high_resolution_clock::now() - Stump).count();
@@ -74,7 +72,6 @@ void GAME::BASE::XRenderLoop(void)
 			Settings.FPS = (boost::chrono::duration_cast<boost::chrono::nanoseconds>(boost::chrono::seconds(1)) / FrameRate).count();
 			Settings.Sync.unlock();
 		}
-		//glfwSwapBuffers(Context);
 	};
 }
 
@@ -86,11 +83,11 @@ glm::dvec2 GAME::BASE::GetCursorPosition(void)
 	//X += SizeX;
 	//Y += SizeY;
 	glfwGetCursorPos(Context, &x, &y);
-	return glm::dvec2(x,SizeY-y);
+	return glm::dvec2(x-SizeX/2., SizeY / 2.-y);
 }
 
 
-void GAME::BASE::Add(boost::shared_ptr<Aux::OpenGL> newObj)
+void GAME::BASE::Add(boost::shared_ptr<Aux::GameObject> newObj)
 { 
 	Sync.lock();
 	OnChange = true;
@@ -98,19 +95,19 @@ void GAME::BASE::Add(boost::shared_ptr<Aux::OpenGL> newObj)
 	Sync.unlock();
 }
 
-void GAME::BASE::Add(size_t ID, boost::shared_ptr<Aux::OpenGL> Obj)
+void GAME::BASE::Add(size_t ID, boost::shared_ptr<Aux::GameObject> Obj)
 {
 	Sync.lock();
 	OnChange = true;
 	auto it=IDObjs.find(ID);
 	if (it == IDObjs.end())
 	{
-		IDObjs.insert(std::pair< size_t, boost::shared_ptr<Aux::OpenGL>>(ID, Obj));
+		IDObjs.insert(std::pair< size_t, boost::shared_ptr<Aux::GameObject>>(ID, Obj));
 	}
 	else
 	{
 		IDObjs.erase(it);
-		IDObjs.insert(std::pair<size_t, boost::shared_ptr<Aux::OpenGL>>(ID, Obj));
+		IDObjs.insert(std::pair<size_t, boost::shared_ptr<Aux::GameObject>>(ID, Obj));
 	};
 	Sync.unlock();
 }
@@ -139,6 +136,11 @@ GAME::BASE::BASE()
 		throw(1337);
 	}
 	SetFPSLimit(60);
+}
+
+GAME::BASE::~BASE()
+{
+	glDeleteBuffers(1, &CameraUBO);
 }
 
 
@@ -174,12 +176,34 @@ unsigned int GAME::BASE::GetFPS(void)
 	return _FPS;
 }
 
+void GAME::BASE::SetCameraPos(glm::vec3 Pos)
+{
+	Sync.lock();
+	Camera.Pos=glm::translate(glm::vec3(Pos.x,Pos.y,-Pos.z));
+	Sync.unlock();
+}
+
+void GAME::BASE::SetCameraAngles(glm::vec3 Angles)
+{
+	glm::vec3 Half =-Angles / 2.;
+	glm::vec3 Radians = glm::vec3(glm::radians(Half));
+	auto Q = glm::quat(cos(Radians.x), 0, 0, -sin(Radians.x))*
+		glm::quat(cos(Radians.y), 0, -sin(Radians.y), 0)*
+		glm::quat(cos(Radians.z), sin(Radians.z), 0, 0);
+	Sync.lock();
+	Camera.Quaternion = Q;
+	Sync.unlock();
+}
+
 void GAME::BASE::CreateWindow(unsigned int Width, unsigned int Height, unsigned int PosX, unsigned int PosY)
 {
 	Context = glfwCreateWindow(Width, Height, "", 0, 0);
-	Sync.lock();
-	Camera.Projection = glm::ortho(-Width / 2, Width / 2, -Height / 2, Height / 2);
-	Sync.unlock();
+	glfwSetWindowUserPointer(Context,this);
+	int height, width; glfwGetWindowSize(Context, &width, &height);
+	//glm::ortho(-(float)Width / 2., (float)Width / 2., -(float)Height / 2., (float)Height / 2.,0.,10000.);
+	//glm::perspectiveFov(45.f, (float)Width, (float)Height, 0.f, 10000.f);
+	Camera.Projection = glm::ortho(-(float)Width / 2., (float)Width / 2., -(float)Height / 2., (float)Height / 2., 0., 10000.);
+	glfwSetWindowSizeCallback(Context,OnWindowReshape );
 	glfwWindowHint(GLFW_VISIBLE, false);
 	ShareContext = glfwCreateWindow(1, 1, "", 0, Context);
 	glfwMakeContextCurrent(ShareContext);
@@ -187,6 +211,18 @@ void GAME::BASE::CreateWindow(unsigned int Width, unsigned int Height, unsigned 
 	{
 		throw(1337);
 	};
+	glGenBuffers(1, &CameraUBO);
+	glBindBuffer(GL_UNIFORM_BUFFER, CameraUBO);
+	glBufferData(GL_UNIFORM_BUFFER, sizeof(GAME::Aux::BASE::Camera), 0, GL_DYNAMIC_DRAW);
 	Run = true;
 	ThreadRender = new boost::thread(boost::bind(&GAME::BASE::XRenderLoop,this));
+}
+
+void GAME::OnWindowReshape(GLFWwindow * window, int NewWidth, int NewHeight)
+{
+	BASE*ptr= reinterpret_cast<BASE*>( glfwGetWindowUserPointer(window));
+	ptr->Sync.lock();
+	ptr->Camera.Projection = glm::ortho(-(float)NewWidth / 2., (float)NewWidth / 2., -(float)NewHeight / 2., (float)NewHeight / 2., 0., 10000.);
+	ptr->Sync.unlock();
+	
 }
--- Game/Base.h
+++ Game/Base.h
@@ -13,30 +13,35 @@ namespace GAME
 			Aux::BASE::Camera Camera;
 			boost::atomic<bool> Run;
 			boost::atomic<bool>OnChange;
-			void OnWindowReshape(GLFWwindow*, int, int);
 			Aux::BASE::Param  Settings;
+			unsigned int CameraUBO;
 			//faster, but it does not suppose id interface
-			std::unordered_set<boost::shared_ptr<Aux::OpenGL>, Aux::HASH>Objs;
+			std::unordered_set<boost::shared_ptr<Aux::GameObject>, Aux::HASH>Objs;
 			//slower, but it is with id control 
-			std::unordered_map< size_t, boost::shared_ptr<Aux::OpenGL>>IDObjs;
+			std::unordered_map< size_t, boost::shared_ptr<Aux::GameObject>>IDObjs;
 			boost::mutex Sync;
 			void XRenderLoop(void);
 			boost::thread*ThreadRender;
+			friend void OnWindowReshape(GLFWwindow*, int, int);
 		public:
 			glm::dvec2 GetCursorPosition(void);
 			//add without id;
-			void Add(boost::shared_ptr<Aux::OpenGL> Obj);
+			void Add(boost::shared_ptr<Aux::GameObject> Obj);
 			//add with specific id;
-			void Add( size_t ID,boost::shared_ptr<Aux::OpenGL> Obj);
+			void Add( size_t ID,boost::shared_ptr<Aux::GameObject> Obj);
 			//remove from id Render List
 			void Remove(size_t ID);
 			//clear all obj in Render List
 			void Remove(void);
 			BASE();
+			~BASE();
 			void SetFPSLimit(unsigned int );
 			void Vsync(bool);
 			GLFWwindow* GetContext(void);
 			unsigned int GetFPS(void);
+			void SetCameraPos(glm::vec3);
+			void SetCameraAngles(glm::vec3);
 			void CreateWindow(unsigned int Width, unsigned int Height,unsigned int PosX,unsigned int PosY);
 		};
+		void OnWindowReshape(GLFWwindow* window, int width, int height);
 }
